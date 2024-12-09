d104a2162751295cc10a1442dcb78831d3475aa8
LiLand6000 <166834733+LiLand6000@users.noreply.github.com>
2024-12-09 12:05:03 +0100

    Tried to fix canvas

--- Assets/AssetsMainStation/Scripts/DisplayManager.cs
+++ Assets/AssetsMainStation/Scripts/DisplayManager.cs
@@ -1,65 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
-// TODO: Setting Display should be reachable from every display - niw error because of indeces
 
 public class DisplayManager : MonoBehaviour
 {
+    public GameObject[] displays; // Array mit allen Display-GameObjects
 
-public GameObject[] displays;  // Array of stripe displays (Images)
-public GameObject[] buttonGroups;  // Array of ButtonGroups: buttons grouped by display belongigns
-
-private int currentDisplayIndex = -1; 
-
-void Start()
-{
-    Debug.Log("Starte Initialisierung...");
-    InitializeDisplaysAndButtons();
-
-    Debug.Log("Displays gefunden: " + displays.Length);
-    Debug.Log("Button-Gruppen gefunden: " + buttonGroups.Length);
-
-    SetActiveDisplay(0); // show first display
-}
-
-// display and button array initialized 
-private void InitializeDisplaysAndButtons()
-{
-    // deactivate all displays and buttons 
-    foreach (var display in displays)
+    public void ShowDisplay(int displayIndex)
     {
-        display.SetActive(false); 
-    }
+        // Alle Displays deaktivieren
+        foreach (GameObject display in displays)
+        {
+            display.SetActive(false);
+        }
 
-    foreach (var buttonGroup in buttonGroups)
-    {
-        buttonGroup.SetActive(false); 
+        // Gewünschtes Display aktivieren
+        if (displayIndex >= 0 && displayIndex < displays.Length)
+        {
+            displays[displayIndex].SetActive(true);
+        }
     }
 }
 
-// change display's index with button click 
-public void OnButtonClick(int newDisplayIndex)
-{
-    Debug.Log("Button clicked! Switching to display: " + newDisplayIndex);
-    SetActiveDisplay(newDisplayIndex);
-}
-
-// change active display
-private void SetActiveDisplay(int newDisplayIndex)
-{
-    // deactivate former display and its buttons
-    if (currentDisplayIndex >= 0)
-    {
-        displays[currentDisplayIndex].SetActive(false);
-        buttonGroups[currentDisplayIndex].SetActive(false); 
-    }
-
-    // activate new display and its buttons
-    currentDisplayIndex = newDisplayIndex;
-    displays[currentDisplayIndex].SetActive(true);
-    buttonGroups[currentDisplayIndex].SetActive(true); 
-}
-}
 
