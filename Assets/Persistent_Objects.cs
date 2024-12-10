3a1ea9e7d562e77be05d00a851679dad6dedc8c5
SgtKeksLF <leandra.j.freese@student.fh-kiel.de>
2024-12-10 11:46:37 +0100

    Greyboxing finish
    Yippie

--- Assets/Persistent_Objects.cs
+++ Assets/Persistent_Objects.cs
@@ -4,31 +4,17 @@ using UnityEngine;
 
 public class Persistent_Objects : MonoBehaviour
 {
-     // Statische Liste für die persistierenden Objekte
-    private static List<GameObject> persistentObjects = new List<GameObject>();
+    //private static Persistent_Objects instance;
 
     private void Awake()
     {
-        // Überprüfen, ob das Objekt der richtigen Art ist (z.B. XR Rig oder XR Interaction Manager)
-        if (gameObject.CompareTag("Persistent"))
+     /*   if (instance != null && instance != this)
         {
-            // Überprüfen, ob dieses Objekt schon in der Liste der persistierenden Objekte ist
-            if (persistentObjects.Contains(gameObject))
-            {
-                // Wenn es bereits existiert, zerstöre dieses Objekt
-                Destroy(gameObject);
-            }
-            else
-            {
-                // Andernfalls füge dieses Objekt zur Liste hinzu und mache es persistent
-                persistentObjects.Add(gameObject);
-                DontDestroyOnLoad(gameObject);
-            }
-        }
-        else
-        {
-            // Wenn das Objekt nicht als persistent markiert ist, lösche es
             Destroy(gameObject);
+            return;
         }
+
+        instance = this;*/
+        DontDestroyOnLoad(gameObject);
     }
 }
